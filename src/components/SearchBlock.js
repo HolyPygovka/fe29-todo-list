fb6843146486f6b73dc62e93ee850e76cbfd3bad
HolyPygovka <r2598509@gmail.com>
2020-06-13 14:57:01 +0300

    les2 start refactor project(searchBlock)

---
+++ src/components/SearchBlock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const SearchBlock = () => {
+    const searchText = 'enter what are you want';
+    return <input disabled placeholder={searchText} />;
+}
+
+export default SearchBlock;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBlock from './components/SearchBlock';
 
-const el = <h1>Реакт Элемент</h1>;
+const Header = () => {
+  const st = {
+    fontSize: '40px',
+    color: 'aqua'
+  }
+  return <h1 style={st} className="Header">Todo List</h1>;
+}
 
-ReactDOM.render(el, document.getElementById('root'));
+const TodoList = () => {
+  const items = ['Learn html', 'Learn css', 'Learn react'];
+  return (
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
+    </ul>
+  );
+}
+
+const App = () => {
+
+  const isLogged = true;
+
+  const loginBlock = <span>sign in</span>;
+  const welcome = <span>Hello</span>;
+
+  return (
+    <div>
+      { isLogged ? welcome : loginBlock }
+      <Header />
+      <SearchBlock />
+      <TodoList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
